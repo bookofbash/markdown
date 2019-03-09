a22bb2e70084c7729720ec66313455ab4ddc5f76
Bashir <harrell.bashir@yahoo.com>
2019-03-09 19:23:53 +0800

    Updates

---
+++ precache-manifest.681a0fb2c1c5cb000f94b304d5ae129f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33525288523c4d40e61f",
+    "url": "/markdown/static/js/runtime~main.e5cb01f6.js"
+  },
+  {
+    "revision": "2effc5502f2cec6e6f52",
+    "url": "/markdown/static/js/main.b23eff84.chunk.js"
+  },
+  {
+    "revision": "f7fbf0ac10ccfc5211a4",
+    "url": "/markdown/static/js/2.a072bdaf.chunk.js"
+  },
+  {
+    "revision": "2effc5502f2cec6e6f52",
+    "url": "/markdown/static/css/main.3522b746.chunk.css"
+  },
+  {
+    "revision": "67267affaa30822a6e6fe9a7cfea33af",
+    "url": "/markdown/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bookofbash/markdown/precache-manifest.e5484232db8045b5d61c6e51940ced26.js"
+  "/markdown/precache-manifest.681a0fb2c1c5cb000f94b304d5ae129f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/bookofbash/markdown/index.html", {
+workbox.routing.registerNavigationRoute("/markdown/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
